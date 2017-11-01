3600e506db47bb864940899a6add7a1b10182104
Pablo Valor <pablovalor89@gmail.com>
2017-10-31 22:37:50 -0300

    Se agrega nuget para acceder a wwwroot

--- api/Startup.cs
+++ api/Startup.cs
@@ -40,6 +40,7 @@ namespace api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStaticFiles();
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
